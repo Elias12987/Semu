7337150c50bd19990bdc4bd262f2d7b22eb6c1c8
Elias12987 <sajjad997113@gmail.com>
2026-06-23 21:23:11 +0330

    Update config.py

--- config.py
+++ config.py
@@ -16,6 +16,6 @@ CARD_NUMBER = os.getenv("CARD_NUMBER", "6219-8619-5197-9607")
 CARD_HOLDER = os.getenv("CARD_HOLDER","علی فرحانی")
 PANEL_URL = os.getenv("PANEL_URL", "https://195.38.19.57:56571")
 PANEL_USERNAME = os.getenv("PANEL_USERNAME", "admin")
-PANEL_PASSWORD = os.getenv("PANEL_PASSWORD", "Admin@123")
+PANEL_PASSWORD = os.getenv("PANEL_PASSWORD,Admin@123")
 
 PANEL_PATH = "/GIiXORcuW6CENAuZNr"
