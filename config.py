2890d43990c7eece5c89d1d64bbad407dc3760dc
Elias12987 <sajjad997113@gmail.com>
2026-06-23 20:34:52 +0330

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ def _parse_admin_ids(raw: str):
     return ids
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
-ADMIN_IDS = _parse_admin_ids(os.getenv("ADMIN_IDS", "123456789"))
+ADMIN_IDS = _parse_admin_ids(os.getenv("ADMIN_IDS", "8471252047","8875652743"))
 DB_PATH = os.getenv("DB_PATH", "bot_database.db")
 CARD_NUMBER = os.getenv("CARD_NUMBER", "6219-8619-5197-9607")
 CARD_HOLDER = os.getenv("CARD_HOLDER","علی فرحانی")
 PANEL_URL = os.getenv("PANEL_URL", "https://195.38.19.57:56571")
-PANEL_USERNAME = os.getenv("PANEL_USERNAME", "")
-PANEL_PASSWORD = os.getenv("PANEL_PASSWORD", "")
+PANEL_USERNAME = os.getenv("PANEL_USERNAME", "admin")
+PANEL_PASSWORD = os.getenv("PANEL_PASSWORD", "Admin@123")
 
 PANEL_PATH = "/GIiXORcuW6CENAuZNr"
